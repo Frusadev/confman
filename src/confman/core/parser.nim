a020c9fd2c741e7a962fb149225dbdb176830c88
Frusadev <frusadev@gmail.com>
2024-06-10 22:43:07 +0000

    Fix parser

--- src/confman/core/parser.nim
+++ src/confman/core/parser.nim
@@ -24,7 +24,46 @@ proc extractList*(s: string) : seq[string] =
 
   return values
 
-proc parseConf*(s: string) : Table[ConfKinds, string] =
+proc parseConf*(raw: string) : Table[ConfKinds, string] =
+  const 
+    confList: seq[string] = @["conf_id", "configs", "main_conf"]
+    confTable: Table[string, ConfKinds] = {
+      "conf_id": CONF_ID,
+      "configs": CONFIGS,
+      "main_conf": MAIN_CONF
+    }.toTable()
+
+  let 
+    input: seq[string] = splitLines(raw)
+  for s in input:
+    var 
+      currentWord: string = ""
+      key: ConfKinds
+      value: string
+
+    if not (':' in s):
+      result[CONF_ID] = s
+    else:
+      var 
+        encounteredColon: bool = false
+      for c in s:
+        if not encounteredColon:
+          if not (c == ':'):
+            currentWord.add(c)
+          else:
+            encounteredColon = true 
+            if currentWord in confList:
+              key = confTable[currentWord]
+              currentWord = ""
+            else: 
+              key = Unknown
+              value = "nil"
+        else:
+          currentWord.add(c)
+      value = currentWord
+    result[key] = value.replace(" ", "")
+
+proc parseLine*(s: string) : Table[ConfKinds, string] =
   const 
     confList: seq[string] = @["conf_id", "configs", "main_conf"]
     confTable: Table[string, ConfKinds] = {
